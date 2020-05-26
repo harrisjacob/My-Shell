2ffd546b30a166ff6ba8d9fd1f65cb3bd436a310
Jacob Harris <harrisjakef@gmail.com>
2020-05-25 21:12:16 -0700

    -a and abs path

--- sources/ls.c
+++ sources/ls.c
@@ -1,28 +1,67 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<sys/types.h>
+#include<sys/stat.h>
 #include<dirent.h>
 #include<stdlib.h>
 #include<errno.h>
+#include<stdbool.h>
 #include<string.h>
+
 
 int main(int argc, char *argv[]){
 
-	char* dName;
-	if(argc == 1) dName = ".";
-	
+	char dirPath[BUFSIZ];
+	getcwd(dirPath, BUFSIZ);
+
+	char* permission = "----------";
+	bool lFlag, aFlag;
+
+
+	lFlag = aFlag = false;
+
+	int opt = 0;
+
+	while((opt = getopt(argc, argv, "la"))!= -1){
+		switch(opt){
+			case 'l':
+				lFlag = true;
+				break;
+			case 'a':
+				aFlag = true;
+				break;
+			default:
+				printf("Unknown flag %c\n", opt);
+				break;
+		}
+	}
+
 	DIR *directory;
-	if(!(directory = opendir(dName))){
+	if(!(directory = opendir(dirPath))){
 		printf("%s: %s\n", argv[0], strerror(errno));
 		return EXIT_FAILURE;
 	}
 
 	struct dirent *dStruct;
-	int count = 0;
 	while((dStruct = readdir(directory))){
-		if(strcmp(dStruct->d_name,".") == 0|| strcmp(dStruct->d_name,"..")==0) continue;
-		if(count > 0 )printf(" ");
-		printf("%s ", dStruct->d_name);
-		count+=1;
+		
+		if(!aFlag){
+			if(strcmp(dStruct->d_name,".") == 0 || strcmp(dStruct->d_name,"..")==0) continue;
+			if((dStruct->d_name)[0] == '.') continue;
+		}
+
+		if(lFlag){
+			struct stat myStat;
+			char* preText = dirPath;
+			char* itemPath = malloc(sizeof(dirPath)+sizeof(dStruct->d_name));
+			sprintf(itemPath, "%s/%s", preText, dStruct->d_name);
+			printf("%s\n", itemPath);
+		
+
+			free(itemPath);
+		}
+
+		printf("%s  ", dStruct->d_name);
 	}
 
 	printf("\n");
