66f8c29e68cdd25046acde115beceb3051fac373
Jacob Harris <harrisjakef@gmail.com>
2020-06-02 20:32:10 -0700

    touch: update access and modification times

--- sources/touch.c
+++ sources/touch.c
@@ -4,6 +4,10 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<errno.h>
+#include<time.h>
+#include<utime.h>
+#include<sys/types.h>
+#include<sys/stat.h>
 
 void usage(){
 	printf("Usage: touch FILE\n");
@@ -27,6 +31,21 @@ int main(int argc, char* argv[]){
 
 	if((fd = open(argv[1], O_CREAT, 00777))<0){
 		fprintf(stderr, "touch: Could not create file %s: %s\n", argv[1], strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	time_t currentTime;
+	if((int) (time(&currentTime)) < 0){
+		fprintf(stderr, "touch: Could not get current system time: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	struct utimbuf timeWrite;
+	timeWrite.actime = currentTime;
+	timeWrite.modtime = currentTime;
+
+	if(utime(argv[1], &timeWrite) < 0){
+		fprintf(stderr, "touch: Failed to write current time to file: %s\n", strerror(errno));
 		return EXIT_FAILURE;
 	}
 
