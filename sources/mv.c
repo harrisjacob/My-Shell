49dd841eb29d6f3d07811e25e6cbda11a16b2374
Jacob Harris <harrisjakef@gmail.com>
2020-06-07 11:12:28 -0700

    mv: Parent directory permissions

--- sources/mv.c
+++ sources/mv.c
@@ -106,11 +106,24 @@ int pathIsDir(char* path){
 	return 0;
 }
 
+int parentAccess(char* path, int mode){
+
+	char* parent = findTailChar(path, '/', 0);
+	if(parent == path) return EXIT_FAILURE;
+	
+	int parentLen = parent - path;
+	char parentPath[parentLen+ 1];
+	strncpy(parentPath, path, parentLen);
+	*(parentPath + parentLen) = '\0';
+
+	return (access(parentPath, mode) == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+}
+
 int handleMV(char* source, char* dest){
 	
 	if(!source || !dest) return EXIT_FAILURE;
 
-	//int dIsDir = 0;
 	char *dPath, *dFile, *sPath, *sFile;
 	dPath = dFile = sPath = sFile = NULL;
 	
@@ -136,14 +149,13 @@ int handleMV(char* source, char* dest){
 	int sRoot = (*source == '/') ? 1 : 0;
 	int dRoot = (*dest == '/') ? 1 : 0;
 
-	//Check if inputs are directories
-
+	
 	if(pathIsDir(source)){
 		free(CWD);
 		return EXIT_FAILURE;  //Source must be a file
 	}
 
-	//Attempt to split source in two
+
 	if(splitPath(&sFile, source)==EXIT_FAILURE){
 		free(CWD);
 		return EXIT_FAILURE;
@@ -155,6 +167,7 @@ int handleMV(char* source, char* dest){
 
 	prefixSz = (sRoot) ? rootLen : CWDLen;
 
+	//Source
 	if(!(sPath = malloc(prefixSz + strlen(source) + 2))){
 		free(sFile);
 		free(CWD);
@@ -169,60 +182,8 @@ int handleMV(char* source, char* dest){
 		
 	strcat(sPath, source);
 
-
-	/*
-	if(strcmp(sPath, sFile) == 0){
-		//If the source char allocs match, a relative file was passed
-		//Reset sPath to path to file
-		free(sPath);
-		sPath = NULL;
-		int prefixSz = (sRoot) ? rootLen : CWDLen;
-
-		if(!(sPath = malloc(prefixSz + strlen(sFile) + 2))){
-			free(sFile);
-			free(CWD);
-			return EXIT_FAILURE;
-		}
-		if(sRoot){
-			strcpy(sPath, root);
-		}else{
-			strcpy(sPath, CWD);
-			strcat(sPath, "/");
-		}
-
-		strcat(sPath, sFile);
-	}else{
-		free(sPath);
-		sPath = NULL;
-		if(*source == '/'){
-			if(!(sPath = malloc(strlen(root) + strlen(source) + 2))){
-				free(sFile);
-				free(CWD);
-				return EXIT_FAILURE;
-			}
-			strcpy(sPath, root);
-		
-		}else{
-			
-			if(!(sPath = malloc(strlen(CWD) + strlen(source) + 2))){
-				free(CWD);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-			strcpy(sPath, CWD);
-			strcat(sPath, "/");
-		}
-		
-		strcat(sPath, source);
-
-	}
-	*/
-
-	printf("\nSource: %s\n", sPath);
-	printf("SFile: %s\n", sFile);
 	
-
-	//Attempt to split dest in two
+	//Destination
 	if(splitPath(&dFile, dest)==EXIT_FAILURE){
 		free(CWD);
 		free(sPath);
@@ -231,7 +192,6 @@ int handleMV(char* source, char* dest){
 	}
 
 	int destIsDir = pathIsDir(dest);
-	prefixSz = (dRoot)? strlen(root) : strlen(CWD);
 
 	if(destIsDir){
 		if(!(dFile = strdup(sFile))){
@@ -278,326 +238,24 @@ int handleMV(char* source, char* dest){
 		strcat(dPath, dest);
 	}
 
+	//printf("\nSource File: %s\n", sFile);
+	//printf("Source Path: %s\n", sPath);
+	//printf("Dest File: %s\n", dFile);
+	//printf("Dest Path: %s\n\n", dPath);
 
 
-	/*
-	if(strcmp(dPath, dFile) == 0){
-		if(destIsDir){
-			
-			free(dFile);
-			dFile = NULL;
-			if(!(dFile = strdup(sFile))){
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-
-			free(dPath);
-			dPath = NULL;
-
-			prefixSz = (*dest == '/')? strlen(root) : strlen(CWD);
-
-			if(!(dPath = malloc(prefixSz + strlen(dest) + strlen(dFile) + 3))){
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				free(dFile);
-			}
-			if(*dest == '/'){
-				strcpy(dPath, root);
-			}else{
-				strcpy(dPath, CWD);
-				strcat(dPath, "/");
-			}
-
-			strcat(dPath, dest);
-			strcat(dPath, "/");
-			strcat(dPath, dFile);
-
-		}else{
-			free(dPath);
-			dPath = NULL;
-			if(!(dPath = malloc(strlen(CWD) + strlen(dFile) + 1))){
-				free(dFile);
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-			strcpy(dPath, CWD);
-			strcat(dPath, "/");
-			strcat(dPath, dFile);
-		}
-	}else{
-		if(destIsDir){
-			free(dFile);
-			dFile = NULL;
-			if(!(dFile = strdup(sFile))){
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-			free(dPath);
-			dPath = NULL;
-			prefixSz = (*dest == '/') ? strlen(root) : strlen(CWD);
-			if(!(dPath = malloc(prefixSz+strlen(dest)+strlen(dFile)+3))){
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				free(dFile);
-			}
-			if(*dest == '/'){
-				strcpy(dPath, root);
-			}else{
-				strcpy(dPath, CWD);
-				strcat(dPath, "/");
-			}
-			
-			strcat(dPath, dest);
-			strcat(dPath,"/");
-			strcat(dPath,dFile);
-
-
-		}else{	
-			free(dPath);
-			dPath = NULL;
-			prefixSz = (*dest == '/') ? strlen(root) : strlen(CWD);
-			if(!(dPath = malloc(prefixSz + strlen(dest) + 2))){
-				free(dFile);
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-
-			if(*dest == '/'){
-				strcpy(dPath, root);
-			}else{
-				strcpy(dPath, CWD);
-				strcat(dPath, "/");
-			}
-		
-			strcat(dPath, dest);
-		}
-
-		
-
-	}
-	*/
-	printf("Dest File: %s\n", dFile);
-	printf("Dest Path: %s\n\n", dPath);
-
-
-
-
-
-
-
-	/*		free(dPath);
-			printf("Dest case 3: Read split\n");
-			free(dPath);
-			dPath = NULL;
-			printf("CWD ------ %s\n", CWD);
-			printf("Root ----- %s\n", root);
-			printf("dest ----- %s\n", dest);
-			if(!(dPath = malloc(strlen(CWD) + strlen(dest) + 1))){
-				free(dFile);
-				free(CWD);
-				free(sPath);
-				free(sFile);
-				return EXIT_FAILURE;
-			}
-	*/
-	free(dFile);
-	free(dPath);
-	free(CWD);
-	free(sPath);
-	free(sFile);
-
-
-
-
-
-
-
-
-
-
-	/*
-	if((dIsDir = pathIsDir(dest))){
-		if(*dest == '/'){
-
-		}else{
-			dPath = strdup(dest);
-		}
-		
-	}*/
-
-	/*
-	//Check if source is a directory
-	if((sIsDir = pathIsDir(source))) return EXIT_FAILURE;
-
-	//Check if destination is a directory
-	dIsDir = pathIsDir(dest);
-
-	if(!dIsDir){
-		//One of two cases:
-		//Valid path but does not end in a directory. Need to check the write permissions of the parent and overwrite the existing file
-		//Invalid dest path provided. Either the file name is new or there was an issue with parent directories.
-
-		if(splitPath(&dPath, &dFile, dest)==EXIT_FAILURE) return EXIT_FAILURE;
-
-	}
-
-
-	//Source
-	if(splitPath(&sPath, &sFile, source)==EXIT_FAILURE){
-		free(dPath);
+	if(parentAccess(sPath, W_OK) == EXIT_FAILURE || parentAccess(dPath, W_OK) == EXIT_FAILURE ){
 		free(dFile);
-		return EXIT_FAILURE;
-	}
-
-	if(!dFile && !(dFile = strdup(sFile))){
 		free(dPath);
+		free(CWD);
 		free(sPath);
 		free(sFile);
-	}
-
-
-	char *CWD;
-	if(!(CWD= getcwd(NULL, 0))){
-		free(dPath);
-	 	free(sPath);
-	 	free(dFile);
-	 	free(sFile);
 		return EXIT_FAILURE;
 	}
 
-	int concatPathSz = strlen(sPath)+strlen(sFile)+2;
-	int rootPathSz = strlen(CWD)+strlen(sFile)+2; 
-	char finSPath[(concatPathSz > rootPathSz) ? concatPathSz : rootPathSz];
-	//Check source permissions
-	if(strcmp(sPath,sFile)==0){
-		//Check write permissions of the CWD
-
-		if(access(CWD, W_OK)!=0){
-			
-			free(dPath);
-		 	free(sPath);
-		 	free(dFile);
-		 	free(sFile);
-		 	free(CWD);
-			return EXIT_FAILURE;
-		}
-
-		strcpy(finSPath,CWD);
-		strcat(finSPath, "/");
-		strcat(finSPath, sFile);
-
-	}else{
-		//Check that write permissions of sPath is ok
-		if(access(sPath, F_OK)!=0 || access(sPath, W_OK)!=0){
-			free(dPath);
-		 	free(sPath);
-		 	free(dFile);
-		 	free(sFile);
-			return EXIT_FAILURE;
-		}
-
-		strcpy(finSPath,sPath);
-		strcat(finSPath, "/");
-		strcat(finSPath,sFile);
 
 
-	}
 
-	
-	int dFileSize = strlen(dFile) + strlen(sFile) + 1;
-	//Check destination permissions
-
-	char finDPath[strlen(dPath)+dFileSize+6];
-	char tempSwitch[strlen(dPath)+dFileSize+2];
-
-	if(dIsDir){
-
-		printf("dPath: %s\n", dPath);
-		if(access(dPath, W_OK)!=0){
-			free(dPath);
-		 	free(sPath);
-		 	free(dFile);
-		 	free(sFile);
-			free(CWD);
-			return EXIT_FAILURE;
-		}
-		printf("dPath: %s\n", dPath);
-		printf("dFile: %s\n", dFile);
-		strcpy(finDPath, dPath);
-		strcat(finDPath, "/");
-		if(strcmp(dFile,"..") != 0  && strcmp(dFile, dPath)!=0){
-			strcat(finDPath, dFile);
-			strcat(finDPath, "/");
-		}
-		strcat(finDPath, sFile);
-		strcat(finDPath, ".tmp");
-
-		strcpy(tempSwitch, dPath);
-		strcat(tempSwitch, "/");
-		if(strcmp(dFile, "..")!=0 && strcmp(dFile, dPath)!=0){
-			strcat(tempSwitch, dFile);
-			strcat(tempSwitch, "/");
-		}
-		strcat(tempSwitch, sFile);
-	
-	}else{
-		//Check write permissions of CWD
-		if(access(CWD, W_OK)!=0){
-			free(dPath);
-		 	free(sPath);
-		 	free(dFile);
-		 	free(sFile);
-			free(CWD);
-			return EXIT_FAILURE;
-		}
-
-		strcpy(finDPath, dPath);
-		strcat(finDPath, "/");
-		strcat(finDPath, dFile);
-		strcat(finDPath, ".tmp");
-
-		strcpy(tempSwitch, dPath);
-		strcat(tempSwitch, "/");
-		strcat(tempSwitch, dFile);
-	}
-	free(CWD);
-	free(sPath);
-	free(sFile);
-	free(dPath);
-	free(dFile);
-
-
-	printf("Source Path: %s\n", finSPath);
-	printf("Destination Path: %s\n", finDPath);
-	printf("Destination Rename: %s\n", tempSwitch);
-	printf("Here\n");
-	
-	
-	if(link(finSPath, finDPath)<0){
-		printf("Fail 1\n");
-		return EXIT_FAILURE;
-	} 
-
-	if(unlink(finSPath)<0){
-		printf("Fail 2\n");
-		return EXIT_FAILURE;
-	}
-
-	if(rename(finDPath, tempSwitch)<0){
-		printf("Fail 3\n");
-		return EXIT_FAILURE;
-	}
-	*/
 	return EXIT_SUCCESS;
 }
 
@@ -605,54 +263,6 @@ int handleMV(char* source, char* dest){
 
 
 int main(int argc, char* argv[]){
-	/*
-	if(pathIsDir("/play")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("/play/directory")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("play")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("play/directory")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("/play/hello")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("play/hello")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("/play/goodbye")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}
-
-	if(pathIsDir("play/goodbye")){
-		printf("Directory\n");
-	}else{
-		printf("Not\n");
-	}*/
 	
 	if(argc < 2){
 		usage();
